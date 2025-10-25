ac8667faba78094586329cc4cf6c517f1a5256b3
Lucas Inocencio <lucasinovo13@gmail.com>
2025-10-25 16:05:59 +0100

    Alterar app

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 import streamlit as st
 from dotenv import load_dotenv
 from src.qr_code_generator import QrcodeGenerator
